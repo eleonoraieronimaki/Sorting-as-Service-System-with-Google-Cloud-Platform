3163a0f0ef18c45518aeaeefbd5a84f31a9968ae
Andreas Savva <andreassavva97@gmail.com>
2022-06-25 03:41:43 +0300

    files to setup kubernetes

--- src/app.py
+++ src/app.py
@@ -205,5 +205,4 @@ def job_statistics(job, workers, reducers):
     return job_stats
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5001))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
